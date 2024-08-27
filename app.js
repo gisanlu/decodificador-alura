4f8792beea3731c48717bcb801b1d1fd572b2c0a
Gisel Sánchez <gisanluc@gmail.com>
2024-08-27 08:32:21 -0600

    decodifica, falta corregir elementos

--- app.js
+++ app.js
@@ -1,26 +1,69 @@
-let mensajeUsuario = document.getElementById("caja-de-texto").value;
+let mensajeUsuario = document.getElementById("caja-de-texto");
+let mostrarMensaje = document.getElementById("mensaje_codificado");
 
+function btnencriptarMensaje() {
+    const textoEncriptado = encripta(mensajeUsuario.value);
+    mostrarMensaje.value = textoEncriptado;
+    mensajeUsuario.value = "";
+    mostrarMensaje.style.backgroundImage = "none";
+}
 
-function compararLetras(arrayMensaje, arrayVocales) {
+function encripta(stringEncriptado) {
     let matrizCodigo = [
-        ["a", "ai"],
         ["e", "enter"],
         ["i", "imes"],
+        ["a", "ai"],
         ["o", "ober"],
-        ["i", "ufat"],
+        ["u", "ufat"],
     ];
 
     stringEncriptado = stringEncriptado.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptado.includes(matrizCodigo[i][0])) {
+            stringEncriptado = stringEncriptado.replaceAll(
+                matrizCodigo[i][0],
+                matrizCodigo[i][1]
+            );
+        }
+    }
+
+    return stringEncriptado;
 }
 
-compararLetras(caracteresMensaje, vocales);
+function btndesencriptarMensaje() {
+    const textoEncriptado = desencripta(mensajeUsuario.value);
+    mostrarMensaje.value = textoEncriptado;
+    mensajeUsuario.value = "";
+}
 
+function desencripta(stringDesencriptado) {
+    let matrizCodigo = [
+        ["e", "enter"],
+        ["i", "imes"],
+        ["a", "ai"],
+        ["o", "ober"],
+        ["u", "ufat"],
+    ];
 
+    stringDesencriptado = stringDesencriptado.toLowerCase();
 
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptado.includes(matrizCodigo[i][0])) {
+            stringDesencriptado = stringDesencriptado.replaceAll(
+                matrizCodigo[i][1],
+                matrizCodigo[i][0]
+            );
+        }
+    }
+
+    return stringDesencriptado;
+}
+
+/*
 function encriptarMensaje() {
     //obtiene el mensaje y lo "separa" para que sea procesado
 
-    
     let caracteresMensaje = [];
 
     let mensajeCodificado = [];
@@ -33,9 +76,7 @@ function encriptarMensaje() {
 
     //crear función para hacer la comparación
 
-    
-
-    /*Si la letra está en la lista de vocal, usar, de lo contrario, no hacerlo
+    //Si la letra está en la lista de vocal, usar, de lo contrario, no hacerlo
     for (let i = 0; i < vocal.length; i++) {
         const vocalComparar = vocal[i];
         if (mensajeSeparado === vocalComparar) {
@@ -43,6 +84,3 @@ function encriptarMensaje() {
             console.log(elementos);
         }
     }*/
-
-    return;
-}
